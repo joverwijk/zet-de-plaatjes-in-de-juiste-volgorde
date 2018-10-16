f5f60bcc343dfecc680222b0f287e4fe379af20d
joverwijk <jarnooverwijk@gmail.com>
2018-10-16 09:23:48 +0200

    + startbestanden

---
+++ start/assets/js/app.js
@@ -0,0 +1,23 @@
+// All global variables
+const imageFolder = "./assets/images/";
+const imagez = [{
+        position: 1,
+        name: 'transformers',
+        source: 'transformers-1.png'
+    },
+    {
+        position: 2,
+        name: 'transformers',
+        source: 'transformers-2.png'
+    },
+    {
+        position: 3,
+        name: 'transformers',
+        source: 'transformers-3.png'
+    },
+    {
+        position: 4,
+        name: 'transformers',
+        source: 'transformers-4.png'
+    }
+];
